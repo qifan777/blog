af2634d2bed522eda481f012a89d30dc705f9dd7
linjiacheng <1507906763@qq.com>
2025-09-19 21:11:26 +0800

    取消侧边菜单

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -55,7 +55,6 @@ export default sidebar({
   "/project/uni-ai/": "structure",
   "/project/spring-ai/": 'structure',
   "/project/mystery-box": 'structure',
-  '/project/college-help':'structure',
   "/knowledge/vite/": [
     { text: "Vite", link: "/knowledge/vite/" },
     {
