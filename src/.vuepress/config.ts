73a2c282ebc32edb220df778b184b83c492095a0
起凡 <1507906763@qq.com>
2025-11-16 20:56:31 +0800

    baseurl

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: '/blog/',
+  base: '/',
   dest: 'src/.vuepress/blog',
   lang: "zh-CN",
   title: "起凡Code闲聊",
